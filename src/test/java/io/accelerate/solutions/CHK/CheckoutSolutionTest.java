a00066c0933422fd67db31cbc0a513306856ff9a
sbx_user1051 <sbx_user1051@169.254.7.73>
2025-08-27 15:54:45 +0000

    Wed Aug 27 15:54:45 UTC 2025

--- src/test/java/io/accelerate/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/io/accelerate/solutions/CHK/CheckoutSolutionTest.java
@@ -1,4 +1,21 @@
 package io.accelerate.solutions.CHK;
 
-public class Check {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class CheckoutSolutionTest {
+    private CheckoutSolution solution = new CheckoutSolution();
+
+    @BeforeEach
+    public void setSolution() { solution = new CheckoutSolution(); }
+
+    @Test
+    public void run() {
+        assertThat(solution.checkout("BCDAAB"), equalTo(180));
+        assertThat(solution.checkout("ADAAB"), equalTo(175));
+        assertThat(solution.checkout("ADAABF"), equalTo(-1));
+    }
 }
+
