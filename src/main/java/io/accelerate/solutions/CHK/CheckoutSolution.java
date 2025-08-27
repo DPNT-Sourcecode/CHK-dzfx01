31fd66084890b1f4f6bdf797bf3104497b9a34c4
sbx_user1051 <sbx_user1051@169.254.7.73>
2025-08-27 15:35:26 +0000

    Wed Aug 27 15:35:26 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -7,7 +7,36 @@ public class CheckoutSolution {
         if(skus.length()<1){
             return -1;
         }
-        
+        Integer count = 0;
+        Integer[] skuRepeated = new Integer[4];
+        String [] skusArr = skus.split(",");
+        for(String sku : skusArr) {
+            switch (sku.trim()) {
+                case "A":
+                    skuRepeated[0]++;
+                    count += 50;
+                    break;
+                case "B":
+                    skuRepeated[1]++;
+                    count += 30;
+                    break;
+                case "C":
+                    skuRepeated[2]++;
+                    count += 20;
+                    break;
+                case "D":
+                    skuRepeated[3]++;
+                    count += 15;
+                    break;
+                default:
+                    return -1;
+            }
+        }
+
+        if(skuRepeated[0]){}
+
+        return count;
     }
 }
+
 
