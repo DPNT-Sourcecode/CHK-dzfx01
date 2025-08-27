76ebdf2a86dcbe78a81245c32e98be8e24b11802
sbx_user1051 <sbx_user1051@169.254.7.73>
2025-08-27 15:50:26 +0000

    Wed Aug 27 15:50:26 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -2,38 +2,44 @@ package io.accelerate.solutions.CHK;
 
 import io.accelerate.runner.SolutionNotImplementedException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
         if(skus.length()<1){
             return -1;
         }
         Integer count = 0;
-        Integer[] skuRepeated = new Integer[4];
+        Map<Character, Integer> skuRepeated = new HashMap<>();
         char [] skusArr = skus.toCharArray();
         for(char sku : skusArr) {
-            switch (sku) {
+            skuRepeated.put(sku, skuRepeated.getOrDefault(sku, 0) + 1);
+        }
+
+        for(Map.Entry<Character, Integer> value : skuRepeated.entrySet()){
+            char sku = value.getKey();
+            int repeated = value.getValue();
+            switch (sku){
                 case 'A':
-                    skuRepeated[0]++;
+                    count+= (repeated / 3) * 130;
+                    count+= (repeated % 3) * 50;
                     break;
                 case 'B':
-                    skuRepeated[1]++;
+                    count+= (repeated / 2) * 45;
+                    count+= (repeated % 2) * 30;
                     break;
                 case 'C':
-                    skuRepeated[2]++;
+                    count+= repeated * 20;
                     break;
                 case 'D':
-                    skuRepeated[3]++;
+                    count+= repeated * 15;
                     break;
                 default:
                     return -1;
             }
         }
-
-        for(int i=0;i<skuRepeated.length;i++){
-            
-        }
-
-
         return count;
     }
 }
+
