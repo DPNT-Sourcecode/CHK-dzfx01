e1a9a044308fe2befb0aee285cda1a972eb08a30
sbx_user1051 <sbx_user1051@169.254.38.109>
2025-09-01 11:35:40 +0000

    Mon Sep 01 11:35:40 UTC 2025

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -20,10 +20,24 @@ namespace BeFaster.App.Solutions.CHK
                 { 'A', [ new int[] { 3, 120 }, new int[] { 5, 200 } ] },
                 { 'B', [ new int[] { 2, 45 } ] },
             };
-            
+
+            if (skus != null)
+            {
+                var count = new Dictionary<char, int>();
+                foreach (var value in skus.ToCharArray())
+                {
+                    var item = count.GetValueOrDefault(value);
+                    if (count.GetValueOrDefault())
+                }
+                }
+            else
+            {
+                return -1;
+            }
         }
     }
 }
+
 
 
 
