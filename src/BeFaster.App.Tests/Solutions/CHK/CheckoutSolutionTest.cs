e2a89ff797812c25fc2aac134977e1fc95924281
sbx_user1051 <sbx_user1051@169.254.38.109>
2025-09-01 11:56:40 +0000

    Mon Sep 01 11:56:40 UTC 2025

--- src/BeFaster.App.Tests/Solutions/CHK/CheckoutSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/CHK/CheckoutSolutionTest.cs
@@ -1,4 +1,4 @@
-using BeFaster.App.Solutions.SUM;
+using BeFaster.App.Solutions.CHK;
 using NUnit.Framework;
 
 namespace BeFaster.App.Tests.Solutions.CHK
@@ -6,11 +6,17 @@ namespace BeFaster.App.Tests.Solutions.CHK
     [TestFixture]
     public class CheckoutSolutionTest
     {
-        [TestCase("AAABCD")]
-        [TestCase("AAABCD")]
-        public void ComputeSum(string input, int )
+        [TestCase("AAABCD", 0)]
+        [TestCase("AAABFD", 0)]
+        [TestCase("AAABEE", 0)]
+        [TestCase("AAABAAD", 0)]
+        [TestCase("A", 0)]
+        [TestCase("ACBB", 0)]
+        [TestCase("", 0)]
+        public void ComputeSum(string input, int output)
         {
-            Assert
+            var result = new CheckoutSolution().Checkout(input);
+            Assert.Equals(output, result);
         }
     }
 }
