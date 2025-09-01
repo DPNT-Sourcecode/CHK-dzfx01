b8ba684b13c38bac9a2e800abb7c4c95bebd9892
sbx_user1051 <sbx_user1051@169.254.38.109>
2025-09-01 11:53:40 +0000

    Mon Sep 01 11:53:40 UTC 2025

---
+++ src/BeFaster.App.Tests/Solutions/CHK/CheckoutSolutionTest.cs
@@ -0,0 +1,16 @@
+using BeFaster.App.Solutions.SUM;
+using NUnit.Framework;
+
+namespace BeFaster.App.Tests.Solutions.CHK
+{
+    [TestFixture]
+    public class CheckoutSolutionTest
+    {
+        [TestCase("AAABCD")]
+        [TestCase("AAABCD")]
+        public void ComputeSum(string input, int )
+        {
+            Assert
+        }
+    }
+}
